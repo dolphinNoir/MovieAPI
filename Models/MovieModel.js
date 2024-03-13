104f053b9181b9dbac1272eeab684d7fd1044966
johnnybasgallop04 <106543268+johnnybasgallop04@users.noreply.github.com>
2024-03-13 19:21:04 +0000

    added in endpoints for finding by title and for imdb ID

---
+++ Models/MovieModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const MovieModel = new mongoose.Schema({
+    title : String,
+    "vote_average": Number,
+    "vote_count": Number,
+    status : String,
+    "release_date": Date,
+    revenue : Number,
+    runtime: Number,
+    adult: Boolean,
+    "backdrop_path" : String,
+    budget : Number,
+    homepage: String,
+    "imdb_id" : String,
+    "original_language" : String,
+    "original_title" : String,
+    overview : String,
+    popularity : Number,
+    "poster_path" : String,
+    tagline : String,
+    genres : String,
+    "production_companies" : String,
+    "production_countries" : String,
+    "spoken_languages" : String
+
+})
+
+module.exports = mongoose.model('MovieAPI', MovieModel, 'MovieAPI')
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT;
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require('cors')
-
+const MovieModel = require('./Models/MovieModel')
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -17,9 +17,63 @@ const db = mongoose.connection;
 db.on('error', (error) => {console.log(error)})
 db.once('open', () => {console.log("Connected to DB")})
 
+//////////////////////////////////////////////////////////
+
 app.get("/", (req,res) => {
-    res.json("Welcome to the UFC Data API, head to the /Fighters endpoint for fighter information, head to the /Events endpoint for event information, enjoy :) ")
+    res.json("Welcome to the Movie API, here you can find a collection of over 1million movies!!! Read our docs to find out how to use the API. ")
 })
+
+//////////////////////////////////////////////////////////
+
+app.get("/Random/:limit", async (req,res) => {
+  const limit = req.params.limit
+  try {
+    const RandomMovies = await MovieModel.aggregate([
+      { $sample: { size: Number(limit) } }
+  ])
+    res.json(RandomMovies)
+
+  } catch (error) {
+    res.status(200).json({message: error.message})
+  }
+
+})
+
+//////////////////////////////////////////////////////////
+
+app.get("/FindByImbdId/:id", async (req,res) => {
+  try { 
+
+    const id = req.params.id
+    const movie = await MovieModel.find({"imdb_id": id})
+
+    res.json(movie)
+    
+  } catch (error) {
+      res.status(200).json({message: error.message})
+  }
+})
+
+//////////////////////////////////////////////////////////
+
+app.get("/FindByTitle/:title", async (req,res) => {
+  try { 
+
+    const title = req.params.title
+    const finalSentence = title.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+
+    const movie = await MovieModel.find({title: finalSentence})
+
+    res.json(movie)
+    
+  } catch (error) {
+      res.status(200).json({message: error.message})
+  }
+})
+
+//////////////////////////////////////////////////////////
+
+
 
 app.listen(port, () => console.log(`started server on port ${port}`));
  
@@ -29,3 +83,4 @@ process.on('SIGINT', () => {
       process.exit(0);
     });
   });
+
